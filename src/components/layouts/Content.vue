4844943d829bae67c74dd1a51cab85c1f5ca3a34
DN_NhanNV <nhannv@mcrew-tech.com>
2019-11-12 17:10:20 +0700

    done user list page

--- src/components/layouts/Content.vue
+++ src/components/layouts/Content.vue
@@ -1,10 +1,21 @@
 <template>
-    <div class="content-wrapper"></div>
+    <div class="content-wrapper p-lr-10">
+      <h3>{{headingTitle}}</h3>
+      <UsersList></UsersList>
+    </div>
 </template>
 
 <script>
+import UsersList from '../users/List';
 export default {
   name: 'Content',
-
+    components: {
+      UsersList,  
+  },
+  data: function(){
+    return{
+      headingTitle: 'Users'
+    }
+  }
 }
 </script>
